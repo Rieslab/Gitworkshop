a83744764c12839ad2ba1741214bf9af7487de0a
Philipp Hoess <philipp.hoess@embl.de>
2018-04-10 11:46:30 +0200

    Questions added by Philipp

--- src/team2/Question.java
+++ src/team2/Question.java
@@ -7,24 +7,27 @@ public class Question {
 
 
 	private ArrayList<String> pool_;
-	
+
 	public Question(){
 		init();
 	}
-	
+
 	private void init(){
 		pool_ = new ArrayList<String>();
-		
+
 		// Add Questions here
 		pool_.add("What is the most boring thing in the lab?");
 		pool_.add("What will be then end of M2?");
 		pool_.add("What never fails to disappoint people?");
+		pool_.add("Why are yeast NPCs so ugly?");
+		pool_.add("Who likes Kostek's music?");
+		pool_.add("When will the 4Pi be ready?");
 
 	}
-	
+
 	public String drawNew(){
-		Random rand = new Random(); 
-		int value = rand.nextInt(pool_.size()); 
+		Random rand = new Random();
+		int value = rand.nextInt(pool_.size());
 		return pool_.get(value);
 	}
 }
